286fea545e1dc928bbfb74c38f8e434a1387cbf0
Frankie Chen <fchen@grapevine6.com>
2023-01-08 23:47:17 -0500

    basic setup

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./styles/base.scss";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+console.log(2131232123)
+console.log(2131232123)
+root.render(<App />);
---
+++ webpack.development.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  mode: "development",
+  entry: "./src/index",
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].[contenthash].js",
+    chunkFilename: "js/[name].[contenthash].js",
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  devtool: "source-map",
+  resolve: {
+    modules: ["./node_modules"],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  devServer: {
+    // contentBase: "./dist",
+    port: 3000,
+    hot: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        // exclude: [/node_modules/],
+      },
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: false,
+      template: "./src/index.html",
+    }),
+    new ForkTsCheckerWebpackPlugin()
+  ],
+};
