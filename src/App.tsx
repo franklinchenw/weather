a7dd91e267358dd89c9eb4da815b61a01792bcc3
Frankie Chen <fchen@grapevine6.com>
2023-01-12 17:56:44 -0500

    remove dist and host app on gh

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Weather } from "./components/pages/weather";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Weather />} />
       </Routes>
